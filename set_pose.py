dc15a4ff05fffa9d8ef152e58b2c115d92fd7d78
silverwings88 <alvarosouza00@gmail.com>
2018-10-31 12:23:50 -0700

    Comments added

--- set_pose.py
+++ set_pose.py
@@ -6,40 +6,49 @@ from math import pow, atan2, sqrt
 
 class turtlebot():
 
+    #Creating our node,publisher and subscriber
     def __init__(self):
-        #Creating our node,publisher and subscriber
+
+        # Start ros node
         rospy.init_node('turtlebot_controller', anonymous=True)
+        
+        # Define topic to publish - robot velocity
         self.velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=10)
+        
+        # Define topic to subscribe - robot position
         self.pose_subscriber = rospy.Subscriber('/turtle1/pose', Pose, self.callback)
+        
+        # Create pose object to store robot position
         self.pose = Pose()
         self.rate = rospy.Rate(10)
 
     #Callback function implementing the pose value received
     def callback(self, data):
+
         self.pose = data
         #print self.pose
         self.pose.x = round(self.pose.x, 4)
         self.pose.y = round(self.pose.y, 4)
 
-    def get_distance(self, goal_x, goal_y):
-        distance = sqrt(pow((goal_x - self.pose.x), 2) + pow((goal_y - self.pose.y), 2))
-        return distance
-
+    
     def move2goal(self):
         
+        # Create pose object to store user defined goal
         goal_pose = Pose()
         
+        # Read input from CLI. Integer for x goal or 'q' to close application
         cmd = raw_input("Set your x goal, or 'q' to exit: ")
         
+        # If read data from CLI is not 'q'
         if cmd != "q":
         
-            
+            # Read x setpoint, y setpoint and tolerance defined by the user
             goal_pose.x = int(cmd, 16)
             goal_pose.y = input("Set your y goal: ")
             distance_tolerance = input("Set your tolerance: ")
             vel_msg = Twist()
 
-
+            
             while sqrt(pow((goal_pose.x - self.pose.x), 2) + pow((goal_pose.y - self.pose.y), 2)) >= distance_tolerance:
 
                 #Porportional Controller
@@ -74,11 +83,12 @@ if __name__ == '__main__':
 
     try:
     
-        #Testing our function
+        # Create turtlebot class
         x = turtlebot()
         
         while True:
     
+            # Move the robot to the position defined by the user in a loop
             x.move2goal()
 
     except rospy.ROSInterruptException: pass
